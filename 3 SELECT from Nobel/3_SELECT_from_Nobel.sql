acf09e778396d396be4afecd9c91585aac0475e2
davidauza.engineer <davidauza01@gmail.com>
2019-12-19 17:28:10 -0500

    FInish 3_SELECT_from_Nobel.sql

--- 3 SELECT from Nobel/3_SELECT_from_Nobel.sql
+++ 3 SELECT from Nobel/3_SELECT_from_Nobel.sql
@@ -43,3 +43,21 @@ SELECT yr, subject, winner
 FROM nobel
 WHERE (subject = 'Medicine' AND yr < 1910)
    OR (subject = 'Literature' AND yr >= 2004);
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE winner LIKE 'Peter Gr_nberg';
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE winner = 'Eugene O''Neill';
+
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner ASC;
+
+SELECT winner, subject
+FROM nobel
+WHERE yr = 1984
+ORDER BY subject IN ('Chemistry', 'Physics'), subject, winner;
